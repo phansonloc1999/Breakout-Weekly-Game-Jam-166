5aa1894c1fe6d45df9b9cb3408fd8d6b9db57f98
phansonloc1999 <phansonloc1999@gmail.com>
2020-09-18 21:19:52 +0700

    Speed ball velocity up after boucing off Paddle outer parts

--- Assets/Scripts/BallCollision.cs
+++ Assets/Scripts/BallCollision.cs
@@ -10,6 +10,12 @@ public class BallCollision : MonoBehaviour
 
     [SerializeField] private float _velocityScale;
 
+    [SerializeField] private BoxCollider2D _paddleCenterCollider;
+
+    [SerializeField] private BoxCollider2D _paddleOuterCollider;
+
+    [SerializeField] private Transform _paddleTransform;
+
     private bool _hasPrevCollisionInSameFrame;
 
     private void Start()
@@ -22,16 +28,32 @@ public class BallCollision : MonoBehaviour
         if (!_hasPrevCollisionInSameFrame)
         {
             _velocityScale = 1f; // Default no scale at all
-            if (other.tag == "Paddle" && _paddleMovement.IsMoving) _velocityScale = 1.2f;
 
-            var dx = Mathf.Abs(transform.position.x - other.transform.position.x) - other.bounds.size.x / 2;
-            var dy = Mathf.Abs(transform.position.y - other.transform.position.y) - other.bounds.size.y / 2;
+            var dx = 0f;
+            var dy = 0f;
+
+            if (other.tag == "PaddleOuter" || other.tag == "PaddleCenter")
+            {
+                if (other.tag == "PaddleOuter" && _paddleMovement.IsMoving) _velocityScale = 1.2f;
+
+                dx = Mathf.Abs(transform.position.x - _paddleTransform.position.x) - (_paddleOuterCollider.bounds.size.x * 2 + _paddleCenterCollider.bounds.size.x) / 2;
+                dy = Mathf.Abs(transform.position.y - _paddleTransform.position.y) - (_paddleOuterCollider.bounds.size.y + _paddleCenterCollider.bounds.size.y) / 2;
+            }
+            else
+            {
+                dx = Mathf.Abs(transform.position.x - other.transform.position.x) - other.bounds.size.x / 2;
+                dy = Mathf.Abs(transform.position.y - other.transform.position.y) - other.bounds.size.y / 2;
+            }
 
             if (dx > dy)
-                _ballMovement.SetVelocity(new Vector2(-_ballMovement.Velocity.x * _velocityScale, _ballMovement.Velocity.y * _velocityScale));
+            {
+                SideBouncing();
+            }
 
             if (dx < dy)
-                _ballMovement.SetVelocity(new Vector2(_ballMovement.Velocity.x * _velocityScale, -_ballMovement.Velocity.y * _velocityScale));
+            {
+                TopBottomBouncing();
+            }
 
             _hasPrevCollisionInSameFrame = true;
 
@@ -44,5 +66,15 @@ public class BallCollision : MonoBehaviour
         yield return new WaitForEndOfFrame();
 
         _hasPrevCollisionInSameFrame = false;
+    }
+
+    private void SideBouncing()
+    {
+        _ballMovement.SetVelocity(new Vector2(-_ballMovement.Velocity.x * _velocityScale, _ballMovement.Velocity.y * _velocityScale));
+    }
+
+    private void TopBottomBouncing()
+    {
+        _ballMovement.SetVelocity(new Vector2(_ballMovement.Velocity.x * _velocityScale, -_ballMovement.Velocity.y * _velocityScale));
     }
 }
